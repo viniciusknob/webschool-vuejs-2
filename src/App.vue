ec38cc299dadf20ae99a7dd340e568b1dbf36e70
Vinícius M. Knob <knob.vinicius@gmail.com>
2020-08-29 16:32:50 -0300

    Lesson 6 - Use directives, md-toolbar and md-menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="phone-viewport">
+
     <md-tollbar>
-      <h1 class="md-title">{{ title }}</h1>
+      <div class="md-toolbar-row">
+        <md-menu>
+          <md-button md-menu-trigger class="md-icon-button" v-on:click.native="toogleLeftSideNav">
+            <md-icon>menu</md-icon>
+          </md-button>
+
+          <md-menu-content>
+            <md-menu-item>My Item 1</md-menu-item>
+            <md-menu-item>My Item 2</md-menu-item>
+            <md-menu-item>My Item 3</md-menu-item>
+          </md-menu-content>
+
+        </md-menu>
+        <h1 class="md-title">{{ title }}</h1>
+      </div>
     </md-tollbar>
   </div>
 </template>
@@ -12,6 +27,11 @@
     data() { // two-way data binding
       return {
         title: 'Ecommerce Webschool with Vue.JS 2'
+      }
+    },
+    methods: {
+      toogleLeftSideNav() {
+
       }
     }
   }
