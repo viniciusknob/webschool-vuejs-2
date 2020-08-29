85ddd077f789c82e87d8426bd6c204c4d742b2fe
Vinícius M. Knob <knob.vinicius@gmail.com>
2020-08-29 16:06:38 -0300

    Lesson 5 - Two-way data binding

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="phone-viewport">
     <md-tollbar>
-      <h1 class="md-title">Ecommerce Webschool with Vue.JS 2</h1>
+      <h1 class="md-title">{{ title }}</h1>
     </md-tollbar>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'WebschoolComponent'
+    name: 'WebschoolComponent',
+    data() { // two-way data binding
+      return {
+        title: 'Ecommerce Webschool with Vue.JS 2'
+      }
+    }
   }
 </script>
 
